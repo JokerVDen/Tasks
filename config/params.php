107ea7c4a782a3c971bdb3a8a6c730e7e4ac8d9e
Joker_V <joker_v_@ukr.net>
2020-01-26 01:47:56 +0200

    Add hidden db parameters

--- config/params.php
+++ config/params.php
@@ -1,7 +1,12 @@
 <?php
+$db = require CONFIG . '/db.php';
+if (file_exists(CONFIG . '/db_web.php')) {
+    $dbWeb = require CONFIG . '/db_web.php';
+    $db = array_merge($db, $dbWeb);
+}
 
 return [
-    'db'     => require CONFIG . '/db.php',
+    'db'     => $db,
     'routes' => require CONFIG . '/route.php',
     'blade'  => require CONFIG . '/blade.php'
 ];
